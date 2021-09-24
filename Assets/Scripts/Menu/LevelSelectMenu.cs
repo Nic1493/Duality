0f893912a1872e9587fe96551e86edb4e5d18b17
dyam1188 <066255860a@gmail.com>
2021-09-23 20:25:51 -0400

    Revert "Moved Coded from UnlockLevel.cs"
    This reverts commit 1e407bd0935363e00037cace77b79563ffd70025.

--- Assets/Scripts/Menu/LevelSelectMenu.cs
+++ Assets/Scripts/Menu/LevelSelectMenu.cs
@@ -5,26 +5,9 @@ public class LevelSelectMenu : Menu
 {
     [SerializeField] Button backButton;
 
-    [SerializeField] IntObject levelClearCount;
-    [SerializeField] Button[] buttons;
-
-    void Start()
-    {
-        EnableLevelButton();
-    }
-
-
     void Update()
     {
         if (Input.GetButtonDown("Cancel"))
             backButton.OnPointerClick(eventData);        
-    }
-
-    void EnableLevelButton()
-    {
-        for (int i = 0; i <= levelClearCount.value; i++)
-        {
-            buttons[i].GetComponent<Button>().enabled = true;
-        }
     }
 }
