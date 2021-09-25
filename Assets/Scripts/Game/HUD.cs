1312ee1c4a8ff6c218b0c534be9d53e0ac636c57
Nic1493 <nic.choy107@gmail.com>
2021-09-25 15:44:47 -0400

    Move counter updates
    - moved MoveCounter display to Level Clear menu (turned Level Clear menu into a prefab)
    - removed HUD from level scenes (to-do: delete HUD-related project files?)

---
+++ Assets/Scripts/Game/HUD.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    void Awake()
+    {
+        FindObjectOfType<Goal>().PlayerInGoalAction += OnPlayerEnterGoal;
+    }
+
+    void OnPlayerEnterGoal(int playerCount)
+    {
+        if (playerCount == 2)
+            enabled = false;
+    }
+}
